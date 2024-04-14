94db07166ec2087ada08b1f27c8e71c3fb4249a6
MengSuyan Lin <alyson26168@gmail.com>
2024-04-14 13:15:25 +0800

    feat: first commit with configuration.

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@theme': path.resolve(__dirname, 'src/utils/theme'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
+});
