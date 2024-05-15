c4a15e52523193d528c1a2c1f6ce7d02a9161070
MengSuyan Lin <alyson26168@gmail.com>
2024-05-15 23:27:32 +0800

    chore: Delete unused import item.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'mapbox-gl/dist/mapbox-gl.css';
 import GlobalStyle from './assets/GlobalStyle';
 import {
   createBrowserRouter,
@@ -39,11 +38,10 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <>
-    <GlobalStyle />
-    <RouterProvider router={router} />
-  </>
-
-  // </React.StrictMode>
+  <React.StrictMode>
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
+  </React.StrictMode>
 );
